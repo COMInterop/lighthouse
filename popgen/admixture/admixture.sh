67033b66fd77f961d56a31d11d7b5eebbd489a51
COMInterop <7071578+COMInterop@users.noreply.github.com>
2025-07-28 13:15:33 -0500

    reorg

--- popgen/admixture/admixture.sh
+++ popgen/admixture/admixture.sh
@@ -2,23 +2,23 @@
 
 #SBATCH --job-name=admix
 #SBATCH --output=%x.txt
-#SBATCH --partition=debug
+#SBATCH --partition=
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=168
+#SBATCH --cpus-per-task=
 
 # Prepare the environment
 
-source /apps/bpike/miniforge3/etc/profile.d/conda.sh
+source /path/to/miniforge3/etc/profile.d/conda.sh
 conda activate popgen
 set -euo pipefail
 
 # Set shell variables
 
-DIR=/data_HPC02/bpike/lh/pan/align/joint/all
-OUT=joint-all-recode-structure
+DIR=
+OUT=
 K=4
-THREADS=168
+THREADS=
 
 # Run ADMIXTURE
 
